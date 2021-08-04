6a63146a0fdde41260c7594d8c7cd8bf1a655749
ittySparrow <ittysparrow@gmail.com>
2021-08-04 10:28:29 +0300

    test port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+// const PORT = process.env.PORT || 3000
+const PORT = 3000;
 
-app.use(express.static(`${__dirname}/dist`))
+app.use(express.static(`${__dirname}/dist`));
 
 app.listen(PORT, function () {
-  console.log(`I'm on port ${PORT}!`)
-})
+  console.log(`I'm on port ${PORT}!`);
+});
